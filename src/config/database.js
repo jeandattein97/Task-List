0eacc3ad16477925a04015bed912f0567b30893a
jeandattein97 <127666184+jeandattein97@users.noreply.github.com>
2025-01-20 23:03:19 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,17 +5,12 @@ import path from 'path';
 import { fileURLToPath } from 'url'; 
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename); 
-// Ajuste o caminho aqui para o arquivo .env na mesma pasta 
+
 dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-
-
 
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-
-
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     host: 'localhost',
